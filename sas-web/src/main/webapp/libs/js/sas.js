acd15a8e6dc5cfec02894792ad907ba2030188c9
geine <bonos2@wisdo.me>
2016-09-29 01:55:52 +0900

    sas.js init

--- sas-web/src/main/webapp/libs/js/sas.js
+++ sas-web/src/main/webapp/libs/js/sas.js
@@ -1,3 +1,18 @@
 /**
- * 
+ * @author genie
+ * @date 2016.09.28
  */
+
+(function($){
+
+    function sasApp(){
+        
+    }
+    
+    sasApp.prototype.init = function(){
+        alert("hi");
+    }
+    
+    $.sasApp = sasApp.prototype;
+    
+})(jQuery);
